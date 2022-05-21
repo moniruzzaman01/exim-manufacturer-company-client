939be9535cdfcd64521f77b0e010eb6cc151236e
moniruzzaman01 <moniruzzamanshakib04@gmail.com>
2022-05-22 02:39:18 +0600

    portfolio page done

--- src/Pages/Portfolio/Portfolio.js
+++ src/Pages/Portfolio/Portfolio.js
@@ -2,8 +2,28 @@ import React from "react";
 
 const Portfolio = () => {
   return (
-    <div>
-      <h2>Portfolio</h2>
+    <div className=" flex justify-center ">
+      <div className=" absolute top-1/3">
+        <div class=" w-fit bg-base-200 shadow-xl rounded-xl">
+          <div className="flex">
+            <div className=" p-10 flex-1">
+              <h2 className=" text-xl">Name: </h2>
+              <h2 className=" text-xl">Email:</h2>
+              <h2 className=" text-xl">Educational Background:</h2>
+            </div>
+            <div className=" p-10 flex-1">
+              <h2 className=" text-xl">Moniruzzaman </h2>
+              <h2 className=" text-xl">moniruzzaman.shakib.1@gmail.com</h2>
+              <h2 className=" text-xl">
+                Student of Electrical and Electronic Engineering
+                <br />
+                level-2 semester-I
+              </h2>
+            </div>
+          </div>
+          <div className="text-center">live site link</div>
+        </div>
+      </div>
     </div>
   );
 };
