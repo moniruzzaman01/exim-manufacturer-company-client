609bed9cba465833ad35c8e466c03a608e480303
moniruzzaman01 <moniruzzamanshakib04@gmail.com>
2022-05-23 23:45:43 +0600

    carousel design change

--- src/Components/Carousel.js
+++ src/Components/Carousel.js
@@ -1,23 +1,42 @@
 import React from "react";
 
 const Carousel = () => {
-  const images = [1, 2, 3, 4, 5, 6];
-
+  const images = [
+    "https://i.ibb.co/hg00N15/carousel1-2.jpg",
+    "https://i.ibb.co/T2wDjX6/carousel1-3.jpg",
+    "https://i.ibb.co/0DNgQvZ/carousel1-4.jpg",
+    "https://i.ibb.co/BZC8f8s/carousel1-1.jpg",
+  ];
   return (
-    <div className="carousel w-full">
-      {images.map((image, key) => (
-        <div key={key} id="slide1" className="carousel-item relative w-full">
-          <img src="" className="w-full" alt="" />
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a href="#slide4" className="btn btn-circle">
-              ❮
-            </a>
-            <a href="#slide2" className="btn btn-circle">
-              ❯
-            </a>
-          </div>
+    <div>
+      <div className="carousel lg:h-[60vh] w-full">
+        <div id="item1" className="carousel-item w-full">
+          <img src={images[2]} className="w-full" alt="" />
         </div>
-      ))}
+        <div id="item2" className="carousel-item w-full">
+          <img src={images[1]} className="w-full" alt="" />
+        </div>
+        <div id="item3" className="carousel-item w-full">
+          <img src={images[0]} className="w-full" alt="" />
+        </div>
+        <div id="item4" className="carousel-item w-full">
+          <img src={images[3]} className="w-full" alt="" />
+        </div>
+      </div>
+      <div className="flex justify-center w-full py-2 gap-2">
+        <a href="#item1" className="btn btn-xs">
+          1
+        </a>
+        <a href="#item2" className="btn btn-xs">
+          2
+        </a>
+        <a href="#item3" className="btn btn-xs">
+          3
+        </a>
+        <a href="#item4" className="btn btn-xs">
+          4
+        </a>
+      </div>
     </div>
   );
 };
